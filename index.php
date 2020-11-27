fb4cec96a2cc0afb89f66f1f2149fce0f9939550
Mariangel Ramirez <mari@Mariangels-MacBook-Pro.local>
2020-11-27 16:10:25 -0700

    adding home page

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+   <meta charset="UTF-8">
+   <title>Galleries Login</title>
+   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet" />
+   <link rel="stylesheet" href="css/index.css" />
+</head>
+
+<body>
+   <header>
+      <div class="wrapper">
+         <div class="item">
+         <input class="loginBtn" type="button" value="Login">
+         </div>
+         <div class="item">
+         <input class="joinnBtn" type="button" value="Join">
+         </div>
+         <div class="item">
+         <input class="searchBar" type="text" placeholder="Search painting or gallery..">
+         </div>
+      </div>
+   </header>
+</body>
+
+</html>
