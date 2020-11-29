e8b6aeb9f0a035364b526649a768e6170ad090b0
Mariangel Ramirez <mariangelrr@hotmail.com>
2020-11-28 22:17:56 -0700

    updates for home page

--- index.php
+++ index.php
@@ -8,23 +8,28 @@
    <meta charset="UTF-8">
    <title>Galleries Login</title>
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <link rel="stylesheet" href="css/index.css" />
 </head>
 
-<body>
-   <header>
-      <div class="wrapper">
-         <div class="item">
-         <input class="loginBtn" type="button" value="Login">
-         </div>
-         <div class="item">
-         <input class="joinnBtn" type="button" value="Join">
-         </div>
-         <div class="item">
-         <input class="searchBar" type="text" placeholder="Search painting or gallery..">
-         </div>
+<body class="container">
+   <div class="main">
+      <div class="loginBtn">
+         <input type="button" value="LOGIN">
       </div>
-   </header>
+      <div class="joinBtn">
+         <input type="button" value="JOIN">
+      </div>
+
+   </div>
+   <div class="searchBar">
+      <input type="text" placeholder="Search painting or gallery..">
+      <button><i class="fa fa-search"></i></button>
+   </div>
 </body>
+
+<footer>
+   <p>Image from Unsplash by Darya Tryfanava</p>
+</footer>
 
 </html>
