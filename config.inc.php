d4bd05205bfcc23a79f9bb16565c995a62a4161b
Brian <brianatuh_3@hotmail.com>
2020-11-30 11:47:36 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -14,7 +14,7 @@
 
 define('DBUSER', "p6d3fldzumd697t8");
 define('DBPASS', "un11bavbn0ccbnev");
-define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" . ";dbname=" . "tl1lklkcndfcb8di");
+define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" . ";dbname=" . "tl1lklkcndfcb8di;charset=utf8mb4;");
 
 // //define('DBHOST', 'localhost');
 // //define('DBNAME', 'art');
