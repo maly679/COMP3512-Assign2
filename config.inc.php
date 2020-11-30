b4d01628fc1cead14c861748781daa6365635df5
Brian <brianatuh_3@hotmail.com>
2020-11-30 09:50:27 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,23 @@
 <?php
 
 $url = 'mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di';
+
 $dbparts = parse_url($url);
 console_log($dbparts);
 
 $hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
+console_log($hostname);
 
-define('DBCONNSTRING', "mysql:host=" . $hostname . ";dbname=" . $database);
+$username = $dbparts['user'];
+console_log($username);
+
+$password = $dbparts['pass'];
+console_log($password);
+
+$database = ltrim($dbparts['path'],'/');
+console_log($database);
+
+define('DBCONNSTRING', "mysql:host=" . $hostname . ";dbname=" . $database . ";charset=utf8mb4;");
 define('DBUSER', $username);
 define('DBPASS', $password);
 
