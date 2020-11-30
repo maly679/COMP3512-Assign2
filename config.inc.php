46d5ac76a223b4cf94a861efa11b5816f9abefe4
Brian <brianatuh_3@hotmail.com>
2020-11-29 20:28:05 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,19 @@
 <?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'art');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+//define('DBHOST', 'localhost');
+//define('DBNAME', 'art');
+//define('DBUSER', 'root');
+//define('DBPASS', '');
+//define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+$url = getenv('JAWSDB_URL');
+$dpparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['host'], '/');
+
+define('DBUSER', $username);
+define('DBPASS', $password);
+define('DBCONNSTRING',"mysql:host=" . $hostname . ";dbname=" . $database);
 ?>
