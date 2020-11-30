5d72aece23f0d2d24f3fceff086ea391c2cdbf1f
maly679 <46073520+maly679@users.noreply.github.com>
2020-11-29 20:49:01 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,19 +1,29 @@
 <?php
-//define('DBHOST', 'localhost');
-//define('DBNAME', 'art');
-//define('DBUSER', 'root');
-//define('DBPASS', '');
-//define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 
-$url = getenv('JAWSDB_URL');
-$dpparts = parse_url($url);
-
+$url = mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di;
+$dbparts = parse_url($url);
 $hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
-$database = ltrim($dbparts['path'], '/');
-
+$database = ltrim($dbparts['path'],'/');
+define('DBCONNECTION', "mysql:host=$hostname;dbname=$database");
 define('DBUSER', $username);
 define('DBPASS', $password);
-define('DBCONNSTRING',"mysql:host=" . $hostname . ";dbname=" . $database);
+// //define('DBHOST', 'localhost');
+// //define('DBNAME', 'art');
+// //define('DBUSER', 'root');
+// //define('DBPASS', '');
+// //define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+
+// $url = getenv('JAWSDB_URL');
+// $dpparts = parse_url($url);
+
+// $hostname = $dbparts['host'];
+// $username = $dbparts['user'];
+// $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'], '/');
+
+// define('DBUSER', $username);
+// define('DBPASS', $password);
+// define('DBCONNSTRING',"mysql:host=" . $hostname . ";dbname=" . $database);
 ?>
