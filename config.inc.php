8105e524882cc4f521c526e3637486bb6b36b2fb
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:13:41 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DBCONNECTION', "mysql:host='kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';dbame='tl1lklkcndfcb8di');
+define('DBCONNECTION', "mysql:host='kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';dbame='tl1lklkcndfcb8di'");
 define('DBUSER', 'p6d3fldzumd697t8');
 define('DBPASS', 'un11bavbn0ccbnev');
 
