55ddd3b67d397b7a8db90e8122ca15b2647df2a5
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-01 19:23:18 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,19 @@
 <?php
 
+
 session_start();
+
 require_once 'config.inc.php';
 require_once 'db-classes.inc.php';
+
+
+function checkIfError() {
+if (isset($_GET['redirect'])) {
+    if ($redirect = 'error') {
+        echo "<div class = 'error'> Incorrect username and password combination entered. Please try again. </div>";
+        }
+    }
+}
 
 if (isset($_GET['email']) && isset($_GET['pass'])) {
 
@@ -16,10 +27,6 @@ if (isset($_GET['email']) && isset($_GET['pass'])) {
         die ($e -> getMessage());
     }
 
-} else {
-
-    echo "Username and password not set successfully.";
-
 }
 
 function checkData($pass, $data) {
@@ -27,64 +34,39 @@ function checkData($pass, $data) {
     if (isset($data) && count($data) > 0) {
         foreach($data as $row) {
             if(password_verify($pass, $row['Pass'])) {
-                echo "username and password enetered successfully";
                 $_SESSION['ID'] = $row['CustomerID'];
                 $_SESSION['status'] = "loggedIn";
                 header('location: index.php');
             } else {
-                echo "incorrect username and password entered.";
+              
+                header("location:login.php?redirect='error'");
+
             }
          }
     } else {
-       echo "Incorrect username and password combination entered";
-       
+
+        header("location:login.php?redirect='error'");
+
     }
 }
 
 
-// //$digest = password_hash( $_POST['pass'], PASSWORD_BCRYPT, ['cost' => 12] );
-// //$userName = $_SESSION[""];
-// $pass = "SELECT Password_sha256 from customerlogon";
 
-// try {
-//     $conn = DatabaseHelper::createConnection(array(
-//         DBCONNSTRING,
-//         DBUSER, DBPASS
-//     ));
+?>
 
-//     $result = DatabaseHelper::runQuery($conn, $pass, null);
-//     $data = $result->fetchAll(PDO::FETCH_ASSOC);
-//     echo "hello";
-// } catch (Exception $e) {
-//     die($e->getMessage());
-// }
-
-// //if ($digest == $password_field_from_database_table //&& emails also match
-// //) {
-//  // we have a match, log the user in
-//  session_start(); // starts the session
-//  // check for favorites
-//  if (isset($_SESSION["favorites"])) {
-//      $_SESSION["favorites"];
-//  }
-//  //header(location: index.php); // redirect the logged in user to the homepage 
-// //}
-
-// ?>
-<!DOCTYPE html>
-<html lang=en>
-
-<head>
-    <title>Login Page</title>
-    <meta charset=utf-8>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet" />
-    <link rel="stylesheet" href="css/login.css" />
-</head>
-
-<body>
-    <!--Retrieved November, 27, 2020. from: https://www.w3schools.com/howto/howto_css_login_form.asp-->
-    <form action="login.php" method="get">
+<!DOCTYPE html>   
+<html>   
+<head>  
+<meta name="viewport" content="width=device-width, initial-scale=1">  
+<title> Login Page </title>  
+<link rel = "stylesheet" href = "css/login.css"> </style>
+</head>    
+<body>    
+   <h1> Login to the Gallery </h1> 
+    <form action="login.php" method="get">  
+        <div class="container">   
         <div class="loginBox">
+        <?=checkIfError();?>
             <label>
                 <p>Email</p>
             </label for="email">
@@ -96,7 +78,8 @@ function checkData($pass, $data) {
             <button type="Submit">Login</button>
             <button type="Submit">Sign up</button>
         </div>
-    </form>
-</body>
 
-</html>
+        </div>   
+    </form>     
+</body>     
+</html>  
