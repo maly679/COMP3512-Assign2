4277f6bf3da2cac6abc5559046e379c0c05f636d
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:26:09 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,22 +1,7 @@
 <?php
 
-
-// define('DBCONNECTION', "mysql:host='kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';dbame='tl1lklkcndfcb8di'");
-// define('DBUSER', 'p6d3fldzumd697t8');
-// define('DBPASS', 'un11bavbn0ccbnev');
-
-
-
-
 define('DBUSER', "p6d3fldzumd697t8");
 define('DBPASS', "un11bavbn0ccbnev");
 define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" . ";dbname=" . "tl1lklkcndfcb8di;charset=utf8mb4;");
-
-// $url = "mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di";
-// $dpparts = parse_url($url);
-// define('DBUSER', $dbparts['user']);
-// define('DBPASS', $dbparts['pass']);
-// define('DBCONNSTRING', "mysql:host=" . $dbparts['host'] . ";dbname=" . ltrim($dbparts['path'],'/') . ";charset=utf8mb4;");
-
 
 ?> 
