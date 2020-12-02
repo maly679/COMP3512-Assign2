1145dd3de7f22a422a39e7428d293a6bb165ab1b
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:11:23 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,20 +1,17 @@
 <?php
-$url = "mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di";
-$dbparts = parse_url($url);
-print_r($dbparts);
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-define('DBCONNECTION', "mysql:host=$hostname;dbname=$database");
-define('DBUSER', $username);
-define('DBPASS', $password);
-echo "host" . $hostname;
-echo "db". $database;
-echo "un" . $username;
-echo "pw" . $password;
-?>
 
 
+define('DBCONNECTION', "mysql:host='kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';dbame='tl1lklkcndfcb8di');
+define('DBUSER', 'p6d3fldzumd697t8');
+define('DBPASS', 'un11bavbn0ccbnev');
 
-
+// $dbparts = parse_url($url);
+// print_r($dbparts);
+// $hostname = $dbparts['host'];
+// $username = $dbparts['user'];
+// $password = $dbparts['pass'];
+// $database = ltrim($dbparts['path'],'/');
+// define('DBCONNECTION', "mysql:host=$hostname;dbname=$database");
+// define('DBUSER', $username);
+// define('DBPASS', $password);
+?> 
