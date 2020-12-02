5e0a3c9c14cb358263c07589d10fa13dcd167b39
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 08:57:03 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,11 +4,6 @@ session_start();
 if (isset($_SESSION['ID']) && isset($_SESSION['status']) ) {
     unset($_SESSION['ID']);
     unset($_SESSION['status']);
-    header('location:login.php');
-} else {
-    echo "user not logged in";
-}
-
-
-
+    header('location:index.php');
+} 
 ?>
