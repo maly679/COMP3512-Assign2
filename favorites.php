e5e2b6b6468dcec506cf456d0db0817b8ad47d17
mbalu028 <44375209+mbalu028@users.noreply.github.com>
2020-12-01 17:28:27 -0700

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+require_once 'config.inc.php';
+require_once 'db-classes.inc.php';
+
 // checks if there is an existing array of favourites
 if (!isset($_SESSION['favorites'])) {
     // initializes the array if the check is true
