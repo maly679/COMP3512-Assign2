9cd70e1f74eab8667680c25528efe384fa893aaa
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:14:35 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -5,13 +5,4 @@ define('DBCONNECTION', "mysql:host='kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.
 define('DBUSER', 'p6d3fldzumd697t8');
 define('DBPASS', 'un11bavbn0ccbnev');
 
-// $dbparts = parse_url($url);
-// print_r($dbparts);
-// $hostname = $dbparts['host'];
-// $username = $dbparts['user'];
-// $password = $dbparts['pass'];
-// $database = ltrim($dbparts['path'],'/');
-// define('DBCONNECTION', "mysql:host=$hostname;dbname=$database");
-// define('DBUSER', $username);
-// define('DBPASS', $password);
 ?> 
