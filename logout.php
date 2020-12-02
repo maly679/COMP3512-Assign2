c033cdb44e03bb4c2bbc2b05726738e82eba09c4
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 09:04:38 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,11 @@ session_start();
 if (isset($_SESSION['ID']) && isset($_SESSION['status']) ) {
     unset($_SESSION['ID']);
     unset($_SESSION['status']);
+    //return to (not logged in) home page.
     header('location:index.php');
-} 
+} else {
+    
+    //catch error, where if user was not logged in, and redirect to login page.
+    heder('location:login.php');
+}
 ?>
