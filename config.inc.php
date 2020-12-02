e830ae40e6d93c7e37c33338b693ee59fe475dc0
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:01:20 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,7 @@
 <?php
 $url = "mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di";
 $dbparts = parse_url($url);
+print_r($dbparts);
 $hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
