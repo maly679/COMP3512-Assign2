faec596e1c5026b84260a7ca1bfb54b4fc9af278
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 16:23:58 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -6,11 +6,17 @@
 // define('DBPASS', 'un11bavbn0ccbnev');
 
 
-$url = "mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di";
-$dpparts = parse_url($url);
-define('DBUSER', $dbparts['user']);
-define('DBPASS', $dbparts['pass']);
-define('DBCONNSTRING', "mysql:host=" . $dbparts['host'] . ";dbname=" . ltrim($dbparts['path'],'/') . ";charset=utf8mb4;");
+
+
+define('DBUSER', "p6d3fldzumd697t8");
+define('DBPASS', "un11bavbn0ccbnev");
+define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" . ";dbname=" . "tl1lklkcndfcb8di;charset=utf8mb4;");
+
+// $url = "mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di";
+// $dpparts = parse_url($url);
+// define('DBUSER', $dbparts['user']);
+// define('DBPASS', $dbparts['pass']);
+// define('DBCONNSTRING', "mysql:host=" . $dbparts['host'] . ";dbname=" . ltrim($dbparts['path'],'/') . ";charset=utf8mb4;");
 
 
 ?> 
