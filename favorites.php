55f35df539734f908b45ce36863d72fa23dfa5a0
mbalu028 <44375209+mbalu028@users.noreply.github.com>
2020-12-01 17:21:28 -0700

    Updated favorites.php 12/1/2020 5:21PM
    12/1/2020 5:21PM

---
+++ favorites.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+// checks if there is an existing array of favourites
+if (!isset($_SESSION['favorites'])) {
+    // initializes the array if the check is true
+    $_SESSION['favorites'] = [];
+}
+
+// retrieves existing favourites
+$fav = $_SESSION['favorites'];
+// and then its adds the passed product id to the array
+$fav[] = $_GET['id'];
+// reserve modified array back to the session state
+$_SESSION['favorites'] = $fav;
+
+// outputs the lists of the logged-in user's favourited paintings
+function outputList($fav)
+{
+    echo "<ul>";
+    foreach ($fav as $f) {
+        echo "<li>";
+        echo "<a ></a>";
+        echo "</li>";
+    }
+    echo "</ul>";
+}
