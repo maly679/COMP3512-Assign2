cdf22f14ef1e9ff4ff102ce0f7d9f7d96199c0f2
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 13:32:35 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 session_start();
 
 if (isset($_SESSION['ID']) && isset($_SESSION['status']) ) {
+
+    //unset() functionality retreived from: https://www.php.net/manual/en/function.unset.php
     unset($_SESSION['ID']);
     unset($_SESSION['status']);
     unset($_SESSION['favorites']);
