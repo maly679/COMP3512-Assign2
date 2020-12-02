7d9ecd36bb58988959c58b5502a43423032b8dab
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 13:22:03 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@ session_start();
 if (isset($_SESSION['ID']) && isset($_SESSION['status']) ) {
     unset($_SESSION['ID']);
     unset($_SESSION['status']);
+    unset($_SESSION['favorites']);
     //return to (not logged in) home page.
     header('location:index.php');
 } else {
