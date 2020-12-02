afd6f281e5babcb6b1c6977227b384e0acc6b8e8
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 15:43:21 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_SESSION['ID']) && isset($_SESSION['status']))
 {
 
-    //unset() functionality retreived from: https://www.php.net/manual/en/function.unset.php
+    //unset() functionality retrieved from: https://www.php.net/manual/en/function.unset.php
     //re-intialize the Session variables pertianing to logged on user's values.
     unset($_SESSION['ID']);
     unset($_SESSION['status']);
