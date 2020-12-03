5d9bc5ff19602289b2ab4c89404dcd995654dc93
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-03 12:14:07 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,13 +1,22 @@
 <?php
 
-$user = "p6d3fldzumd697t8";
-$pass = "un11bavbn0ccbnev";
-$host = "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbname = "tl1lklkcndfcb8di";
-$charSetTrail = ";charset=utf8mb4;";
-define('DBUSER', $user);
-define('DBPASS', $pass);
-define('DBCONNSTRING', "mysql:host=" . $host . ";dbname=" . $dbname . $charSetTrail);
+// $user = "p6d3fldzumd697t8";
+// $pass = "un11bavbn0ccbnev";
+// $host = "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+// $dbname = "tl1lklkcndfcb8di";
+// $charSetTrail = ";charset=utf8mb4;";
+// define('DBUSER', $user);
+// define('DBPASS', $pass);
+// define('DBCONNSTRING', "mysql:host=" . $host . ";dbname=" . $dbname . $charSetTrail);
 
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+define('DBCONNECTION', "mysql:host=$hostname;dbname=$database");
+define('DBUSER', $username);
+define('DBPASS', $password);
 
 ?>
