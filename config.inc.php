b69e3c456ea34c3a4bee8f71e485beaa47b2dff8
Jwalker457 <jwalk457@mtroyal.ca>
2020-12-03 10:44:05 -0700

    update config

--- config.inc.php
+++ config.inc.php
@@ -16,11 +16,11 @@ define('DBUSER', "p6d3fldzumd697t8");
 define('DBPASS', "un11bavbn0ccbnev");
 define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" . ";dbname=" . "tl1lklkcndfcb8di;charset=utf8mb4;");
 
-// //define('DBHOST', 'localhost');
-// //define('DBNAME', 'art');
-// //define('DBUSER', 'root');
-// //define('DBPASS', '');
-// //define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+// define('DBHOST', 'localhost');
+// define('DBNAME', 'art');
+// define('DBUSER', 'root');
+// define('DBPASS', '');
+// define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 
 
 
@@ -44,4 +44,3 @@ define('DBCONNSTRING', "mysql:host=" . "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.
 // //define('DBUSER', 'root');
 // //define('DBPASS', '');
 // //define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
-?>
