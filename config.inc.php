2e225af9bf953b6668e409723e5b2361161a2ca9
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-03 13:08:53 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,22 +1,8 @@
 <?php
 
-// $user = "p6d3fldzumd697t8";
-// $pass = "un11bavbn0ccbnev";
-// $host = "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-// $dbname = "tl1lklkcndfcb8di";
-// $charSetTrail = ";charset=utf8mb4;";
-// define('DBUSER', $user);
-// define('DBPASS', $pass);
-// define('DBCONNSTRING', "mysql:host=" . $host . ";dbname=" . $dbname . $charSetTrail);
-
 $url = getenv('JAWSDB_URL');
-
-print_r($url);
-$url2 = 'mysql://p6d3fldzumd697t8:un11bavbn0ccbnev@kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tl1lklkcndfcb8di;charset=utf8mb4;';
-echo $url2;
-  
-  
 $dbparts = parse_url($url);
+
 $hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
@@ -24,6 +10,5 @@ $database = ltrim($dbparts['path'],'/');
 define('DBUSER', $username);
 define('DBPASS', $password);
 define('DBCONNSTRING', "mysql:host=$hostname;dbname=$database");
-
 
 ?>
