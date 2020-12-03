e5496d747a0ee3253a59a911992b96e758e50b25
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-02 17:39:20 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -3,9 +3,10 @@
 $user = "p6d3fldzumd697t8";
 $pass = "un11bavbn0ccbnev";
 $host = "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbname = "tl1lklkcndfcb8di;charset=utf8mb4;";
+$dbname = "tl1lklkcndfcb8di";
+$charSetTrail = ";charset=utf8mb4;";
 define('DBUSER', $user);
 define('DBPASS', $pass);
-define('DBCONNSTRING', "mysql:host=" . $host . ";dbname=" . $dbname);
+define('DBCONNSTRING', "mysql:host=" . $host . ";dbname=" . $dbname . $charSetTrail);
 
 ?> 
