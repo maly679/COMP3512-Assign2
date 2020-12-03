b19fa4ee67d43c5200c5ea088d8ed7eaeebfa99a
mbalu028 <44375209+mbalu028@users.noreply.github.com>
2020-12-03 15:35:15 -0700

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -3,15 +3,17 @@ session_start();
 require_once 'config.inc.php';
 require_once 'db-classes.inc.php';
 
+/*
 // checks if there is an existing array of favourites
 if (!isset($_SESSION['favorites'])) {
     // initializes the array if the check is true
     $_SESSION['favorites'] = [];
 }
+*/
 
 // retrieves existing favourites
 $fav = $_SESSION['favorites'];
-//print_r($fav);
+print_r($_SESSION['favorites']);
 
 try
 {
