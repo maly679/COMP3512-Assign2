2ea1a63f05673873bdb7eefa6d7378a180810aaf
mbalu028 <44375209+mbalu028@users.noreply.github.com>
2020-12-04 06:07:54 -0700

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -5,6 +5,8 @@ require_once 'db-classes.inc.php';
 require_once 'favorites-helpers.php';
 
 //NOTE: REMOVE DUPES
+
+// if the get of delete is set then deletes the selected painting from favorites
 if (isset($_GET['delete'])) {
     foreach ($_SESSION['favorites'] as $key => $rmv) {
         if ($rmv == $_GET['delete']) {
@@ -12,11 +14,14 @@ if (isset($_GET['delete'])) {
         }
     }
 }
+
+// if deleteAll is set then delete all from favorites
 if (isset($_GET['deleteAll'])) {
     unset($_SESSION['favorites']);
 }
 
 outputDeleteAll();
+
 try {
     $conn = DatabaseHelper::createConnection(array(
         DBCONNSTRING,
