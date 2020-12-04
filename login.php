991668e06d3ef9bd26278ad1e66a49ebf52970d8
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-04 14:23:30 -0700

    Update login.php

--- login.php
+++ login.php
@@ -5,13 +5,16 @@ session_start();
 require_once 'config.inc.php';
 require_once 'db-classes.inc.php';
 
+
 //Function is invoked below in markup; this occurs only if the user entered an incorrect username and password combination.
 function checkIfError()
 {
     if (isset($_GET['redirect']))
-    {
-        if ($_GET['redirect'] == 'error')
+    {      
+
+        if ($_GET['redirect'] == "'error'")
         {
+
             echo "<div class = 'error'> Incorrect username and password combination entered. Please try again. </div>";
         }
     }
@@ -28,10 +31,8 @@ if (isset($_GET['email']) && isset($_GET['pass']))
             DBUSER,
             DBPASS
         ));
-        $sql = "select UserName, CustomerID, Pass FROM customerlogon WHERE UserName = ?";
-        $result = DatabaseHelper::runQuery($conn, $sql, array(
-            $_GET['email']
-        ));
+        $customerGate = new CustomerLogon($conn);
+        $result = $customerGate->getByUserName($_GET['email']);
         $data = $result->fetchAll(PDO::FETCH_ASSOC);
         checkData($_GET['pass'], $data);
     }
@@ -60,7 +61,7 @@ function checkData($pass, $data)
             {
 
                 //redirect to error page if incorrect user entry detected.
-                header("location:login.php?redirect='error'");
+                header("location:login.php?redirect=error");
 
             }
         }
@@ -68,7 +69,7 @@ function checkData($pass, $data)
     else
     {
         //redirect to error page if incorrect user entry detected.
-        header("location:login.php?redirect='error'");
+        header("location:login.php?redirect=error");
 
     }
 }
@@ -87,7 +88,7 @@ function checkData($pass, $data)
         <div class="container">   
         <div class="loginBox">
         <!-- if error occurs, based on query string value, output error here. -->
-        <?=checkIfError(); ?>
+        <?=checkIfError();?>
             <label>
                 <p>Email</p>
             </label for="email">
