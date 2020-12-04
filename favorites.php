f76ec812113c4b62c239a1dc981442a5530015d5
mbalu028 <44375209+mbalu028@users.noreply.github.com>
2020-12-03 18:49:54 -0700

    Update favorites.php

--- favorites.php
+++ favorites.php
@@ -47,7 +47,7 @@ try {
             foreach ($data as $row) {
                 // echo $row['title'];
                 // echo $row['imagefilename'];
-?><form action="favorites.php?rmvP='<?= $row['PaintingID'] ?>''" method="get">
+?><form action="favorites.php?rmvP='<?= $row['PaintingID'] ?>'" method="get">
                     <?= outputList($row); ?>
                     <button type="submit">Delete Painting</button>
                     </div>
