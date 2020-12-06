49a7ca117130016e0a75bf729874c5d6e3fbde9b
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-05 21:46:43 -0700

    Update home-logged-in.php

--- home-logged-in.php
+++ home-logged-in.php
@@ -135,10 +135,11 @@ function getThreshholdEnd($yow)
 //process user info
 function displayUserData($dataID)
 {
+    $comma= ',';
     if (isset($dataID))
     {
         echo "<h2> Welcome " . $dataID[0]['firstname'] . "</h2>";
-        echo ("<p>" . $dataID[0]['firstname'] . " " . $dataID[0]['lastname'] . "</p>" . "<p>" . $dataID[0]['city'] . "," . "</p>" . "<p>" . $dataID[0]['country'] . "</p>");
+        echo ("<p>" . $dataID[0]['firstname'] . " " . $dataID[0]['lastname'] . $comma . "</p>" . "<p>" . $dataID[0]['city'] . "</p>" . "<p>" . $dataID[0]['country'] . "</p>");
     }
 }
 ?>
