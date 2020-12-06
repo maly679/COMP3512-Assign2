08755b50e7e7d7fce8f6b9d40a5e02143e201765
Mariangel Ramirez <mariangelrr@hotmail.com>
2020-12-05 17:28:24 -0700

    minor update; trying to link css file

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ if (isset($_POST['name'])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Galleries Login</title>
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet" />
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
    <link rel="stylesheet" href="css/index.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body class="container">
