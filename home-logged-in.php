71eeba5046adc2790df445e2fdea229f72d70420
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-05 21:42:59 -0700

    Update home-logged-in.php

--- home-logged-in.php
+++ home-logged-in.php
@@ -6,7 +6,7 @@ require_once 'db-classes.inc.php';
 
 //function definitions
 
-
+//output paintings in a formatted manner
 function outputFormattedPainting($data) {
 
 for ($i = 0;$i < count($data);$i++)
@@ -138,7 +138,7 @@ function displayUserData($dataID)
     if (isset($dataID))
     {
         echo "<h2> Welcome " . $dataID[0]['firstname'] . "</h2>";
-        echo ("<p>" . $dataID[0]['firstname'] . " " . $dataID[0]['lastname'] . "</p>" . "<p>" . $dataID[0]['city'] . "</p>" . "<p>" . $dataID[0]['country'] . "</p>");
+        echo ("<p>" . $dataID[0]['firstname'] . " " . $dataID[0]['lastname'] . "</p>" . "<p>" . $dataID[0]['city'] . "," . "</p>" . "<p>" . $dataID[0]['country'] . "</p>");
     }
 }
 ?>
