32ee9b344a0ca3b19bd75a4404654707c4e8647a
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-10 12:00:29 -0700

    Update home-logged-in.php

--- home-logged-in.php
+++ home-logged-in.php
@@ -13,6 +13,7 @@ include 'home-logged-in-helpers.php';
    <meta charset="utf-8" />
    <title>Assignment 1</title>
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="css/home-loggedin.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
