b255e26c5b6335f1ac032da5a90578ec4fae5ea5
maly679 <46073520+maly679@users.noreply.github.com>
2020-12-10 11:37:23 -0700

    Update home-logged-in-helpers.php

--- home-logged-in-helpers.php
+++ home-logged-in-helpers.php
@@ -1,279 +1,387 @@
-<?php
-require_once 'config.inc.php';
-require_once 'db-classes.inc.php';
+body {
+	background-image: url("../images/unsplash-main.jpg");
+	font-family: Open Sans, sans-serif;
+	/* font-size: 0.8rem; */
+	overflow-x: hidden;
+	margin-right: 29px;
+  }
+  
+  /* body {
+	  background-color: steelblue;
+	  font-family: Open Sans, sans-serif;
+	  font-size: 0.8rem;
+	} */
+	.largeImgView {
+		height: 100%;
+		width: 100%;
+	}
+  .container {
+	display: grid;
+	grid-template-columns: 50% 50%;
+	grid-template-rows: 250px 400px minmax(400px, auto);
+	grid-gap: 10px;
+	color: #444;
+  }
+  
+  /* .container {
+		display: grid;
+		padding: 40px;
+		grid-gap: 10px;
+		color: #444;
+		grid-template-areas:
+		  "header header header"
+		  "welcome browse browse"
+		  "favorites likes likes";
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-template-rows: 6.5rem 25rem auto;
+	 } */
+  
+  .a {
+	grid-area: header;
+  }
+  
+  .b {
+	grid-area: welcome;
+  }
+  
+  .c {
+	grid-area: browse;
+	display: grid;
+	overflow: auto;
+  }
+  
+  .d {
+	grid-area: favorites;
+  }
+  
+  .e {
+	grid-area: likes;
+  }
+  
+  .box {
+	background-color: steelblue;
+	color: black;
+	border-radius: 5px;
+	padding: 20px;
+	font-size: 150%;
+  }
+  
+  .h {
+	grid-column: 1 / span 3;
+	text-align: center;
+	color: white;
+  }
+  
+  h2,
+  h3 {
+	text-align: center;
+  }
+  
+  .WelcomeUser {
+	grid-column: 1 / span 1;
+	grid-row-start: 2;
+	grid-row-end: 3;
+	text-align: center;
+  }
+  
+  .searchFavorite {
+	grid-column: 2 / span 1;
+	grid-row-start: 2;
+	grid-row-end: 3;
+  }
+  
+  .Results {
+	grid-column: 2 / span 1;
+	grid-row-start: 3;
+	grid-row-end: 4;
+  }
+  
+  .searchFavoritesBox {
+	/* height: 60px; */
+	width: 200px;
+	font-size: 16px;
+  }
+  
+  button {
+	border-radius: 5px;
+	color: black;
+	display: inline-block;
+	padding: 2px 8px;
+	background: white;
+	border-style: solid;
+	border-width: 1px;
+	text-decoration: none;
+	height: 25px;
+  }
+  
+  #Favorites {
+	grid-column: 1 / span 1;
+	grid-row-start: 3;
+	grid-row-end: 4;
+  }
+  
+  li {
+	display: inline-block;
+	padding: 25px;
+	width: 250px;
+	align-items: center;
+  }
+  
+  .navBtn {
+	float: left;
+	display: block;
+	color: white;
+	text-decoration: none;
+	list-style: none;
+	cursor: pointer;
+	padding: 20px;
+  }
+  
+  .navItems > div > div > ul > li {
+	display: inline;
+	width: 0px;
+	color: white;
+	list-style: none;
+	padding: 0px;
+  }
+  
+  #logo {
+	float: right;
+	width: 300px;
+  }
+  
+  .navItems > div > div > ul {
+	padding: 0;
+	margin: 0;
+  }
+  
+  .navBtn:hover {
+	background-color: steelblue;
+  }
+  
+  #menuIcon,
+  .toggler {
+	display: none;
+  }
+  
+  i {
+	font-style: normal;
+  }
+  
+  .footer {
+	grid-area: footer;
+	text-align: center;
+	color: white;
+	padding: 20px;
+	width: 100%;
+  }
+  
+  /* Purposedly leaving underline on to indicate a link */
+  
+  .footer > p > a {
+	color: white;
+  }
+  
+  @media screen and (max-width: 800px) {
+	/* .container {
+		display: grid;
+		grid-template-rows: 8em 22em 30em;
+		grid-template-areas:
+		  "header"
+		  "form"
+		  "info";
+		grid-template-columns: 1fr;
+		grid-gap: 10px;
+	  } */
+	.navContainer {
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	  z-index: 1;
+	}
+	.navBtn {
+	  float: none;
+	}
+	.toggler {
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  z-index: 2;
+	  cursor: pointer;
+	  width: 50px;
+	  height: 50px;
+	  opacity: 0;
+	  display: block;
+	}
+	#menuIcon {
+	  display: block;
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  z-index: 0;
+	  cursor: pointer;
+	  width: 50px;
+	  height: 50px;
+	  margin: 10px;
+	  font-size: 20px;
+	}
+	#logo {
+	  float: right;
+	  margin: 10px;
+	  width: 200px;
+	}
+	.navItems {
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 0;
+	  overflow: hidden;
+	  display: flex;
+	  align-items: center;
+	  justify-content: center;
+	}
+	.navItems > div {
+	  background: rgba(0, 0, 0, 0.8);
+	  width: 200vw;
+	  height: 100%;
+	  display: flex;
+	  flex: none;
+	  align-items: center;
+	  justify-content: center;
+	}
+	.navItems > div > div {
+	  text-align: center;
+	  max-width: 90vw;
+	  max-height: 100vh;
+	  opacity: 0;
+	  transition: opacity 0.4s ease;
+	}
+	.navItems > div > div > ul > li {
+	  list-style: none;
+	  color: white;
+	  padding: 1rem;
+	}
+	.navItems > div > div > ul > li > a {
+	  color: inherit;
+	  text-decoration: none;
+	  transition: color 0.4s ease;
+	}
+	/* Show menu on toggle on*/
+	.navItems .toggler:checked {
+	  visibility: visible;
+	}
+	.toggler:checked ~ .navItems {
+	  height: 100%;
+	}
+	.toggler:checked ~ .navItems > div > div {
+	  opacity: 1;
+	  transition: opacity 0.4s ease;
+	}
+	.fa {
+	  font-size: 20px;
+	}
+	.button {
+	  width: 100px;
+	}
+	.searchContainer input[type="text"] {
+	  width: 300px;
+	}
+	.container {
+		display: grid;
+		padding: 20px;
+		grid-template-columns:100%;
+		grid-template-rows: auto auto auto auto auto;
+		grid-gap: 10px;
+		color: #444;
+	  }
 
-//function definitions
 
+	  .h {
+		grid-column: 1 / span 1;
+		text-align: center;
+		color: white;
+		grid-row-start: 1;
+		grid-row-end: 2;
+	  }
 
-//retrieve the count of object data
-function getCountofObject($data)
-{
-    $count = 0;
-    foreach ($data as $key => $value)
-    {
-        $count++;
-    }
-    return $count;
-}
+	  .WelcomeUser {
+		grid-column: 1 / span 1;
+		grid-row-start: 2;
+		grid-row-end: 3;
+		text-align: center;
+	  }
 
-//retrieve paintings from list that contains first 15 in table, if there aren't 15 recommended paintings generated by the regular
-//paintings you may like feature
+	  .largeImgView {
+		  height: 98%;
+		  width: 98%;
+	  }
+	  
+	  .searchFavorite {
+		grid-column: 1 / span 1;
+		grid-row-start: 3;
+		grid-row-end: 4;
+	  }
+	  .Results {
+		grid-column: 1 / span 1;
+		grid-row-start: 5;
+		grid-row-end: 6;
+	  }
+	  #Favorites {
+		grid-column: 1 / span 1;
+		grid-row-start: 4;
+		grid-row-end: 5;
+	  }
+  }
+  
+  /* For smaller mobile screens */
+  
+  @media screen and (max-width: 400px) {
+	.container {
+		display: grid;
+		padding: 20px;
+		grid-template-columns:100%;
+		grid-template-rows: auto 20% auto auto auto;
+		grid-gap: 10px;
+		color: #444;
+	  }
+	  .largeImgView {
+		height: 98%;
+		width: 98%;
+	}
+	
 
-function consolidateDataPaintingsMayLike($dataFirst15, $dataPaintingsMayLike)
-{
+	  .h {
+		grid-column: 1 / span 1;
+		text-align: center;
+		color: white;
+		grid-row-start: 1;
+		grid-row-end: 2;
+	  }
 
-    if (isset($dataPaintingsMayLike) && isset($dataFirst15))
-    {
-        $countPaintings = getCountofObject($dataPaintingsMayLike);
-        $regCount = 0;
-        while ($countPaintings < 15)
-        {
-
-            $dataPaintingsMayLike[] = $dataFirst15[$regCount];
-            $countPaintings++;
-            $regCount++;
-        }
-        return $dataPaintingsMayLike;
-
-    }
-}
-
-//Output painting list 
-function outputFormattedPainting($data)
-{
-    echo "<ul>";
-    for ($i = 0;$i < count($data);$i++)
-    {
-
-        echo "<li> <img src='images/paintings/square-medium/" . $data[$i]['ImageFileName'] . ".jpg'/> 
-                        " . "<br>" . $data[$i]['Title'] . "</li>";
-
-    }
-    echo "</ul>";
-}
-
-// Obtain the threshhold of start date
-function getThreshholdStart($yow)
-{
-
-    if ($yow < 1400)
-    {
-        return 0;
-    }
-    else
-    {
-        if ($yow > 1400 && $yow < 1550)
-        {
-            return 1400;
-        }
-        else
-        {
-            if ($yow > 1550 && $yow < 1700)
-            {
-
-                return 1550;
-            }
-            else
-            {
-                if ($yow > 1700 && $yow < 1875)
-                {
-                    return 1700;
-
-                }
-                else
-                {
-                    if ($yow > 1875 && $yow < 1945)
-                    {
-                        return 1875;
-                    }
-                    else
-                    {
-                        return 1945;
-                    }
-                }
-            }
-        }
-    }
-}
-
-// Obtain the threshhold of end date
-function getThreshholdEnd($yow)
-{
-
-    if ($yow < 1400)
-    {
-        return 1400;
-    }
-    else
-    {
-        if ($yow > 1400 && $yow < 1550)
-        {
-            return 1550;
-        }
-        else
-        {
-            if ($yow > 1550 && $yow < 1700)
-            {
-
-                return 1700;
-            }
-            else
-            {
-                if ($yow > 1700 && $yow < 1875)
-                {
-                    return 1875;
-
-                }
-                else
-                {
-                    if ($yow > 1875 && $yow < 1945)
-                    {
-                        return 1945;
-                    }
-                    else
-                    {
-                        return 2020;
-                    }
-                }
-            }
-        }
-    }
-}
-
-// Process user info
-function displayUserData($dataID)
-{
-    if (isset($dataID))
-    {
-        echo "<h2> Welcome " . $dataID[0]['firstname'] . "!";
-        echo ("<p> <strong> <i>" . $dataID[0]['firstname'] . " " . $dataID[0]['lastname'] . " </p>" . "<p>" . $dataID[0]['city'] . ", </p>" . "<p>" . $dataID[0]['country'] . "</p> </i> </strong> </h2>");
-    }
-}
-
-//display info of user
-function displayUserInfo()
-{ // Verify user logged in
-    if (isset($_SESSION['ID']))
-    {
-
-        try
-        {
-            $conn = DatabaseHelper::createConnection(array(
-                DBCONNSTRING,
-                DBUSER,
-                DBPASS
-            ));
-            $customerGate = new Customer($conn);
-            //Query For user info to process welcome user section
-            $dataID = $customerGate->getByID($_SESSION['ID']);
-            //displayDataForUser
-            displayUserData($dataID);
-            $conn = null;
-        }
-        catch(PDOException $e)
-        {
-            die($e->getMessage());
-        }
-
-    }
-}
-
-//process generating paintings the user may like
-function displayDataPaintingsMayLike($ArtistID, $YoWStart, $YoWEnd, $dataFirst15)
-{
-    // Obtain the paintings that user may like, based on first favorite Artist ID and threshhold identified above.
-    try
-    {
-        $conn = DatabaseHelper::createConnection(array(
-            DBCONNSTRING,
-            DBUSER,
-            DBPASS
-        ));
-
-        $paintingGate = new PaintingDB($conn);
-        // Obtain necessary paintings user may like, based on above set values obtained from favorite
-        $dataPaintingsMayLike = $paintingGate->getAllForArtistandEraMayLike($ArtistID, $YoWStart, $YoWEnd);
-        $countPaintings = getCountofObject($dataPaintingsMayLike);
-        // If the paintings count is less than 10, process the outputting to retrieve values from the first 15 in table, to make it meet the threshhol.
-        if (isset($dataPaintingsMayLike) && $countPaintings < 10)
-        {
-            $dataPaintingsMayLike = consolidateDataPaintingsMayLike($dataFirst15, $dataPaintingsMayLike);
-        }
-        $conn = null;
-        // Process the display pattern (odd even, or 0) pertaining to the paintings in order to display the first painting in the middle, $i = odd number paintings on the right column of the showPaintings grid
-        //and $i = even number paintings on the left, for formatting purposes
-        outputFormattedPainting($dataPaintingsMayLike);
-
-    }
-    catch(PDOException $e)
-    {
-        die($e->getMessage());
-    }
-}
-
-//use search feature to generate favorite in large image view
-function processCheckSearch($getSearch)
-{
-
-        echo "<div class='box' id ='Favorites'>";
-        $imageFound = false;
-        if (isset($_SESSION['favorites']) && !empty($_SESSION['favorites']))
-        {
-
-        foreach ($_SESSION['favorites'] as $key => $value)
-        {
-
-            if ($value['Title'] == $getSearch)
-            {
-                echo "<h2> " . $value['Title'] . "</h2>";
-                echo " <img class = 'largeImgView' src='images/paintings/square/" . $value['ImageFileName'] . ".jpg'/ height='700' width='710'>";
-                $imageFound = true;
-                break;
-        }
-    }
-}
-        if (!$imageFound)
-        {
-            echo "<h3>No Result Found<h3> <i> Please try searching for a Title again! </i></h3>";
-
-        }
-}
-
-//generate the favorites
-function processRegFavorites()
-{
-    // If no search entered, display favorites if present
-    echo "<div class='box' id ='Favorites'>";
-    echo "<h2>Your Favorites</h2>";
-    if (isset($_SESSION['favorites']) && !empty($_SESSION['favorites']))
-    {
-        outputFormattedPainting($_SESSION['favorites']);
-
-    }
-
-    echo "</div>";
-    echo "</div>";
-}
-
-// Get the first 15 paintings from the table to use for various scenarios
-
-try
-{
-    $conn = DatabaseHelper::createConnection(array(
-        DBCONNSTRING,
-        DBUSER,
-        DBPASS
-    ));
-
-    $paintingGate = new PaintingDB($conn);
-
-    $dataFirst15 = $paintingGate->getTop15();
-    $conn = null;
-}
-catch(PDOException $e)
-{
-    die($e->getMessage());
-}
-
-
-?>
+	  .WelcomeUser {
+		grid-column: 1 / span 1;
+		grid-row-start: 2;
+		grid-row-end: 3;
+		text-align: center;
+	  }
+	  
+	  .searchFavorite {
+		grid-column: 1 / span 1;
+		grid-row-start: 3;
+		grid-row-end: 4;
+	  }
+	  .Results {
+		grid-column: 1 / span 1;
+		grid-row-start: 5;
+		grid-row-end: 6;
+	  }
+	  #Favorites {
+		grid-column: 1 / span 1;
+		grid-row-start: 4;
+		grid-row-end: 5;
+	  }
+	  
+	  
+  }
+  
